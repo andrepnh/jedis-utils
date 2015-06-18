03d10c90a8badbbee4ac5dba8192bdc4f0f042bd
André Pinheiro <andrepinheiro@quickmobile.com.br>
2015-06-18 19:35:35 -0300

    Results that support conversion of strings to some primitive types

---
+++ src/main/java/com/andrepnh/jedis/utils/LongResult.java
@@ -0,0 +1,16 @@
+package com.andrepnh.jedis.utils;
+
+import java.util.function.Function;
+
+public class LongResult extends ForwardConvertableResult<Long> {
+
+    public LongResult() {
+        super(Long.class);
+    }
+
+    @Override
+    protected Function<String, Long> getConverter() {
+        return Converters.LONG;
+    }
+    
+}
