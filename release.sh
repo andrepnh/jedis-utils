3049029c0d38253f7f846bf0016a4b1eb7f291df
André Pinheiro <andrepinheiro@quickmobile.com.br>
2015-07-20 21:00:18 -0300

    Running maven with the release profile

--- release.sh
+++ release.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 version=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -Ev '(^\[|Download\w+:)' | cut -d'-' -f 1)
-mvn versions:set -DnewVersion=$version
-mvn deploy scm:tag
+echo "Updating POM to version $version"
+mvn -P release versions:set -DnewVersion=$version
+echo "Deploying to repository"
+mvn -P release deploy scm:tag
