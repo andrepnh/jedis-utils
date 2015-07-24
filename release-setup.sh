2a88fdc2d0b80cf60466dcaf3071b3c5290cd816
André Pinheiro <andrepinheiro@quickmobile.com.br>
2015-07-23 22:14:15 -0300

    Tagging the repository now triggers a release

--- release-setup.sh
+++ release-setup.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in secring.gpg.enc -out secring.gpg -d
-openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in pubring.gpg.enc -out pubring.gpg -d
-openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in settings.xml.enc -out settings.xml -d
-openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in jedis-util-github.enc -out jedis-util-github -d
-cp settings.xml ~/.m2/settings.xml
-ssh-agent -s
-ssh-add jedis-util-github
-git config --global user.email "andrepnh@users.noreply.github.com"
-git config --global user.name "maven release plugin"
+if [ ! -z "$TRAVIS_TAG" ]; then
+	openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in secring.gpg.enc -out secring.gpg -d
+	openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in pubring.gpg.enc -out pubring.gpg -d
+	openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in settings.xml.enc -out settings.xml -d
+	cp settings.xml ~/.m2/settings.xml
+fi
